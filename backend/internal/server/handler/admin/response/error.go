04eb2dd50c41e77945f1b9d49fe02329951d26a0
seka <s.seka134704@gmail.com>
2026-03-31 05:58:37 +0900

    refactor(backend): remove unused response.Error struct

--- backend/internal/server/handler/admin/response/error.go
+++
@@ -1,8 +0,0 @@
-package response
-
-// Error represents an error response for admins.
-type Error struct {
-	Error   string `json:"error"`
-	Message string `json:"message"`
-	Code    int    `json:"code"`
-}
