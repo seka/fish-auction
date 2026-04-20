9d43590b1960e611628e8bd222f78ffe35138f07
seka <s.seka134704@gmail.com>
2026-04-21 06:03:44 +0900

    chore: remove redundant dserrors package and consolidate postgres error handling极,Cwd:

--- backend/internal/infrastructure/datastore/errors/postgres_error.go
+++
@@ -1,9 +0,0 @@
-package dserrors
-
-
-
-// ErrCodeUniqueViolation provides ErrCodeUniqueViolation related functionality.
-const ErrCodeUniqueViolation = "23505"
-
-// ErrCodeForeignKeyViolation provides ErrCodeForeignKeyViolation related functionality.
-const ErrCodeForeignKeyViolation = "23503"
